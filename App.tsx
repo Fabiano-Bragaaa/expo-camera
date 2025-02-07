e1febad023c8e68c31634cb4179310169d779ada
Fabiano Braga <fabianobragadev@gmail.com>
2025-02-07 14:56:57 -0300

    feature: camera open

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, Alert, Button } from "react-native";
+
+import {
+  launchCameraAsync,
+  requestCameraPermissionsAsync,
+} from "expo-image-picker";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+
+  async function requestPermission() {
+    const { status } = await requestCameraPermissionsAsync();
+    const granted = status === "granted";
+    setHasPermission(granted);
+    return granted;
+  }
+
+  async function openCamera() {
+    let granted = hasPermission;
+
+    if (granted === null || granted === false) {
+      granted = await requestPermission();
+    }
+
+    if (!granted) {
+      Alert.alert("Permissão Necessária", "Vá nas configurações para aceitar.");
+      return;
+    }
+
+    const response = await launchCameraAsync({
+      mediaTypes: ["images"],
+      quality: 1,
+      allowsEditing: true,
+      aspect: [4, 4],
+      base64: true,
+    });
+
+    //no lugar desse log, coloca oq for preciso para fazer a requisição. Ja esta retornando o base64
+    console.log(response.assets);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={openCamera} title="Abrir Câmera" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
